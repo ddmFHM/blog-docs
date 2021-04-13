84894018e09cab5f02e6fb239356f99f3633f323
ddmFHM <ddmFHM@163.com>
2021-04-13 15:47:38 +0800

    首页 & 主题色样式 & ico

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  title: 'Hello',
-  description: `ddmFHM's technical documentation station`
+  title: 'ddmFHM',
+  description: `ddmFHM's technical documentation station`,
+  head: [
+    ['link', { rel: 'icon', href: '/logo.ico' }],
+    ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css' }],
+    ['link', { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css" }]
+  ]
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,18 @@
-# ddmFHM docs
+---
+home: true
+heroImage: /logo.png
+heroText: ddmFHM
+tagline: 技术积累文档站
+actionText: Start
+actionLink: /zh/guide/
+features:
+- title: 前端笔记
+  details: 学习如春起之苗, 不见其增, 日有所长。
+- title: 算法
+  details: 合抱之木,生于毫末;
+           九层之台,起于累土;
+           千里之行,始于足下。
+- title: 视野拓展
+  details: 博观而约取, 厚积而薄发。
+footer: Copyright © 2021 ddmfhm.com. All rights reserved.
+---
